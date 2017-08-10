b3a4aa88c1df8621ba32c763411d109079e7be4a
rqtx <igormiranda5@gmail.com>
2017-08-10 10:17:31 -0300

    Back to pacaur

--- arch/installPackages.sh
+++ arch/installPackages.sh
@@ -4,8 +4,8 @@ echo "Checking for system updates..."
 sudo pacman -Syu
 
 ##### Install Packages through pacman #####
-sudo pacman -Sy --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol
+sudo pacman -S --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol
 
 ##### Install Packages through pacaur #####
-yaourt --needed --noconfirm --noedit deepin-screenshot sublime-text-dev veracrypt acroread armory-bin kompare-git gitkraken qmplay2 bitcoin-core minergate
+pacaur -S --needed --noconfirm --noedit deepin-screenshot sublime-text-dev veracrypt acroread armory-bin kompare-git gitkraken qmplay2 bitcoin-core minergate
 
