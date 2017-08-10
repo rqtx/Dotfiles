bbd770e3a9e89234a07e674d16bf3efda08a3cdc
rqtx <igormiranda5@gmail.com>
2017-08-10 04:19:27 -0300

    ibus pulseaudio pavucontrol

--- arch/installPackages.sh
+++ arch/installPackages.sh
@@ -4,7 +4,7 @@ echo "Checking for system updates..."
 sudo pacman -Syu
 
 ##### Install Packages through pacman #####
-sudo pacman -Sy --needed --noconfirm neovim redshift wget curl git filezilla
+sudo pacman -Sy --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol
 
 ##### Install Packages through pacaur #####
 pacaur -Sy --needed --noconfirm --noedit deepin-screenshot sublime-text-dev veracrypt acroread armory-bin kompare-git gitkraken qmplay2 bitcoin-core minergate
