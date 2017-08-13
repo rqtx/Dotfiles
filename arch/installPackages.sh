65ac88f64b22b8d4d765adc3cd8b428deec70096
rqtx <igormiranda5@gmail.com>
2017-08-13 15:04:25 -0300

    p7zip

--- arch/installPackages.sh
+++ arch/installPackages.sh
@@ -4,7 +4,7 @@ echo "Checking for system updates..."
 sudo pacman -Syu
 
 ##### Install Packages through pacman #####
-sudo pacman -S --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol
+sudo pacman -S --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol p7zip
 
 ##### Install Packages through pacaur #####
 pacaur -S --needed --noconfirm --noedit deepin-screenshot sublime-text-dev veracrypt acroread armory-bin kompare-git gitkraken qmplay2 bitcoin-core minergate
