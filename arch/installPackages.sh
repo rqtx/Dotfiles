7928fede81b0894262ac10a10362cf8ffbf91948
rqtx <rqtx@protonmail.com>
2017-10-02 22:25:51 -0300

    tmux added

--- arch/installPackages.sh
+++ arch/installPackages.sh
@@ -4,7 +4,7 @@ echo "Checking for system updates..."
 sudo pacman -Syu
 
 ##### Install Packages through pacman #####
-sudo pacman -S --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol p7zip
+sudo pacman -S --needed --noconfirm neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol p7zip tmux
 
 ##### Install Packages through pacaur #####
 pacaur -S --needed --noconfirm --noedit deepin-screenshot sublime-text-dev veracrypt acroread armory-bin kompare-git gitkraken qmplay2 bitcoin-core minergate
