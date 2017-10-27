ee177bc9ae83d181f7b7256e8d237fbe8bde8879
rqtx <rqtx@protonmail.com>
2017-10-27 08:24:16 -0200

    Update executor.sh

--- executor.sh
+++ executor.sh
@@ -17,14 +17,6 @@ echo "==> Updating Mirror list..."
 sudo pacman-mirrors -g --needed --noconfirm
 echo "done"
 
-echo "==> Updating Archlinux keyring..."
-sudo pacman -S --needed --noconfirm archlinux-keyring 
-echo "done"
-
-echo "==> Adding  GPG keys..."
-gpg --keyserver keys.gnupg.net --recv-keys 702353E0F7E48EDB
-echo "	-> ncurses5-compat-libs GPG key added"
-
 echo "==> Checking for system updates..."
 sudo pacman -Syu --needed --noconfirm
 echo "done"
