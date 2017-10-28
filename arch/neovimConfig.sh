a13d51fb21b2b43ad16fd9387fb92ccddbdd6db6
rqtx <rqtx@protonmail.com>
2017-10-28 10:43:15 -0200

    Update neovimConfig.sh

--- arch/neovimConfig.sh
+++ arch/neovimConfig.sh
@@ -2,8 +2,9 @@
 #######
 ## Do not forget to run :PlugInstal inside neovim to install all packages
 ## If YouCompleteme do not run properly compile it with:
-#	cd ~/.vim/bundle/YouCompleteMe
+#	cd ~/.local/shared/nvim/plugged/youcompleteme
 #	./install.py --clang-completer
+#   and install ncurses5-compat-libs
 #######
 
 mkdir ~/.config/nvim
