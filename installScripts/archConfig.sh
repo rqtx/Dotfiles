b8611d7f2f0e1bee09cf7bd073b4633debf7e8b9
Igor Fernandes Miranda <igormiranda5@gmail.com>
2018-01-20 16:30:38 -0200

    Removed unnecessary files

--- installScripts/archConfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo cp ../config/arch/i3-sensible-terminal /usr/bin/
-cp ../config/arch/configi3 ~/.i3/config
-cp ../config/arch/configTermite ~/.config/termite/config
-cp ../config/arch/tmux.conf ~/.tmux.conf
-cp ../config/arch/Xresources ~/.Xresources
-
-xrdb -merge ~/.Xresources
