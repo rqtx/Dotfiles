73af990759e7ffa4f830ab905cd17dc1f533d01e
Igor Fernandes Miranda <igormiranda5@gmail.com>
2018-01-22 13:07:53 -0200

    Updated installScripts

--- installScripts/archInstallPackages.sh
+++ installScripts/archInstallPackages.sh
@@ -6,7 +6,11 @@ echo "	-> ncurses5-compat-libs GPG key added"
 echo "done"
 
 echo "==> Install Packages through pacman"
-sudo pacman -S --needed neovim redshift wget curl git filezilla ibus pulseaudio pavucontrol p7zip tmux termite base-devel cmake ibus xclip stow
+pkgPacman=("neovim" "redshift" "wget" "curl" "git" "ibus" "pulseaudio" "pavucontrol" "p7zip" "tmux" "termite" "base-devel" "cmake" "ibus" "xclip" "stow")
+for i in "${pkgPacman[@]}"
+do
+  sudo pacman -S --needed $i
+done
 echo "done"
 
 echo "==> Install Packages through pacaur"
@@ -15,10 +19,16 @@ if [ ! -n "$(pacman -Qs pacaur)" ]; then
 	sh ./AURhelpers.sh
 fi
 
-pacaur -S --needed --noedit deepin-screenshot sublime-text-dev veracrypt acroread armory-bin kompare-git gitkraken qmplay2 bitcoin-core cppcheck ctags python-pip ncurses5-compat-libs npm 
+# pacaur install
+pkgPacaur=("acroread" "adobe-source-code-pro-fonts" "cppcheck" "ctags" "deepin-screenshot" "sublime-text-dev" "veracrypt" "kompare-git" "qmplay2" "python-pip" "ncurses5-compat-libs" "npm")
+
+for i in "${pkgPacaur[@]}"
+do 
+  pacaur -S --needed --noedit $i 
+done
 echo "done"
 
-echo "==> Installing Extra"
+echo "==> Installing extra"
 sudo pip install cmakelint
 sudo pip install bandit
 sudo pip install jedi
