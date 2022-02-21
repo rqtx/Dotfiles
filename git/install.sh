d312597af7ae0e04c76fb2a814a915a98e9efea6
Igor Fernandes Miranda <igormiranda5@gmail.com>
2022-02-21 20:04:44 -0300

    update

--- git/install.sh
+++ git/install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-files=(".gitconfig")
-for file in "${files[@]}"
-do
-  rm -fv ~/"$file"
-done
+rm -fv ~/gitconfig
+rm -fv ~/.local/bin/git_diff_wrapper
+
 mkdir -p $HOME/.local/bin
 stow --verbose=2 -t $HOME gitconfig
 stow --verbose=2 -t $HOME/.local/bin gitdiff
