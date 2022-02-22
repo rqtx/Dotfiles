c2b18c9b00f96241adada54db2ac3b52c9ac35d0
Igor Fernandes Miranda <igormiranda5@gmail.com>
2022-02-21 22:02:45 -0300

    fixed neovim

--- neovim/install.sh
+++ neovim/install.sh
@@ -14,7 +14,6 @@ mkdir -vp ~/.config/nvim
 stow --verbose=2 -t ~/.config/nvim nvim
 
 sudo rm -rfv /usr/share/nvim/runtime/ftplugin/
-sudo stow --verbose=2 -t /usr/share/nvim/runtime/ runtime
 
 ###Vim Required dirs
 mkdir -p ~/.vim/tmp/swap
