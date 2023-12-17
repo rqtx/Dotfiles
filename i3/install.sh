378c665c3f2e030fa8d700f429079c5afd636ae0
Igor Fernandes Miranda <rqtx@protonmail.com>
2023-12-17 20:11:05 -0300

    i3

--- i3/install.sh
+++ i3/install.sh
@@ -9,3 +9,5 @@ done
 mkdir -p ~/.config/i3;
 stow --verbose=2 -t ~/.config/i3 i3statusbar
 stow --verbose=2 -t ~/.i3/ i3-config
+
+yay -S --needed --answerclean=All --nodiffmenu --noconfirm ulauncher rofi
