0f2b78a7abc5a4d3a4d3ac9c841c6a06d5ec464b
Igor Fernandes Miranda <rqtx@protonmail.com>
2023-12-17 23:18:52 -0300

    i3

--- i3/install.sh
+++ i3/install.sh
@@ -8,6 +8,8 @@ done
 
 mkdir -p ~/.config/i3;
 stow --verbose=2 -t ~/.config/i3 i3statusbar
+stow --verbose=2 -t ~/.config/i3 i3-config
 stow --verbose=2 -t ~/.i3/ i3-config
 
 yay -S --needed --answerclean=All --nodiffmenu --noconfirm ulauncher rofi
+git clone git@github.com:maledorak/dmenu-hotkeys.git ~/.dmenu-hotkeys
