ddc826700393221a31dcdf718070bbb515de6f87
Igor Fernandes Miranda <rqtx@protonmail.com>
2023-12-23 20:52:51 -0300

    neovim

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# https://www.lazyvim.org/installation
 
+rm -rf ~/.config/nvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 files=("keymaps.lua")
-for file in "${files[@]}"
-do
-  rm -fv ~/.config/nvim/lua/config/"$file"
+for file in "${files[@]}"; do
+	rm -fv ~/.config/nvim/lua/config/"$file"
 done
 mkdir -p ~/.config/nvim/lua/config
 mkdir -p ~/.config/nvim/lua/plugins
