69a7ca467aa3ee979afa0bcfa62372f8de1bef15
Igor Fernandes Miranda <rqtx@protonmail.com>
2024-06-23 00:42:48 -0300

    installScripts

--- installScripts/archInstallPackages.sh
+++ installScripts/archInstallPackages.sh
@@ -3,18 +3,16 @@
 echo "==> Install Packages through pacman"
 pkgPacman=("alacritty" "redshift" "docker" "wget" "curl" "git" "ibus" "pipewire" "pipewire-alsa" "pipewire-pulse" "pavucontrol" "podman" "p7zip" "tmux" "base-devel" "cmake" "xclip" "stow" "clang" "neovim " "npm" "starship" "just" "ttf-font-awesome")
 
-for i in "${pkgPacman[@]}"
-do
-  sudo pacman -S --needed --noconfirm $i
+for i in "${pkgPacman[@]}"; do
+	sudo pacman -S --needed --noconfirm $i
 done
 echo "done"
 
 echo "==> Install Packages through yay"
 # yay install
-pkgyay=("adobe-source-code-pro-fonts" "aws-cli" "bash-completion" "brave-bin" "cppcheck" "code" "ctags" "deepin-screenshot" "firefox" "google-chrome" "veracrypt" "virt-manager" "kompare-git" "qmplay2" "python-pip" "powerline-go" "perl-critic" "stow" "timeshift" "jslint" "xflux" "sass-lint" "yamllint")
+pkgyay=("adobe-source-code-pro-fonts" "aws-cli" "bash-completion" "brave-bin" "cppcheck" "code" "ctags" "deepin-screenshot" "firefox" "google-chrome" "veracrypt" "virt-manager" "kompare-git" "qmplay2" "python-pip" "powerline-go" "perl-critic" "stow" "timeshift" "jslint" "xflux" "sass-lint" "yamllint" "ulauncher", "obsidian", "kubelet", "syncthing", "net-tools", "fuse-overlayfs", "qemu-full")
 
-for i in "${pkgyay[@]}"
-do 
-  yay -S --needed --answerclean=All --nodiffmenu --noconfirm $i 
+for i in "${pkgyay[@]}"; do
+	yay -S --needed --answerclean=All --noconfirm $i
 done
 echo "done"
